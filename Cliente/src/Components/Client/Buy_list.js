9d14fa9a450c66cddb2182761461216a693ba13e
Ikegox <reyes.606@hotmail.com>
2019-09-04 21:00:45 -0500

    buy_list avance

--- Cliente/src/Components/Client/Buy_list.js
+++ Cliente/src/Components/Client/Buy_list.js
@@ -1,38 +1,71 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import {getUsername} from '../../store/username/reducer';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles({
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
 
-class Buy_list extends React.Component {
-  constructor(props){
-    super(props)
 
-    this.getBuys = this.getBuys.bind(this);
-    this.getBuys()
-  }
 
-  getBuys(){
-    fetch ("/bill/getBill", {
-      method: 'POST',
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({username:this.props.username})
-    })
-    .then(res=>res.json())
-    .then(res => {
-      console.log(res)
-    })
+export function createCard(list){
+  
+  console.log(list)
+
+  return(<Card >
+    <CardContent>
+      <Typography  color="textSecondary" gutterBottom>
+        Word of the Day
+      </Typography>
+      
+    </CardContent>
+  </Card>)
+}
+
+
+
+function Buy_list(props) {
+  const classes = useStyles(); 
+
+  fetch ("/bill/getBill", {
+    method: 'POST',
+    headers: {
+      Accept: "application/json, text/plain, */*",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({username:props.username})
+  })
+  .then(res=>res.json())
+  .then(res => {
     
-  }
 
-  render(){      
     return (
-    <div className='buy_list'>       
-      <h1>buy list</h1>
-    </div>);
-  }
+      <div className='buy_list'>       
+        <h1>buy list</h1>
+        {createCard(res)}
+      </div>);
+  })
+  
+
+  
 }
 
 
