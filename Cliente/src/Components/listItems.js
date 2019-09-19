45b14578060831498618e3c88f3b226ba9ead4ad
jempool <jem.suarez@correounivalle.edu.co>
2019-09-18 22:39:54 -0500

    Reportes mejores compradores y cumpleaños, ruta añadida

--- Cliente/src/Components/listItems.js
+++ Cliente/src/Components/listItems.js
@@ -80,6 +80,14 @@ export const secondaryListItemsAdmin = (
   <div>
 
     <ListSubheader inset>Reports</ListSubheader>
+
+    <ListItem button component={Link} to="/Admin_page/buyers">
+      <ListItemIcon>
+        <AssignmentIcon />
+      </ListItemIcon>
+      <ListItemText primary="Best buyers" />
+    </ListItem>
+
     <ListItem button component={Link} to="/Admin_page/trending">
       <ListItemIcon>
         <AssignmentIcon />
