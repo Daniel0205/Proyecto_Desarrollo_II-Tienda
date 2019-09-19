f5f972eb61288ad188a6acd117e5265b21600b88
jempool <jem.suarez@correounivalle.edu.co>
2019-09-19 10:40:35 -0500

    Ajustes en la apariencia del home

--- Cliente/src/Components/Home.js
+++ Cliente/src/Components/Home.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import updateType from '../store/type/action'
 import updateUsername from '../store/username/action'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 import Store from './Client/Store';
+import Logo from "../Images/logo.png";
 
-
-
- class Greeting extends React.Component {
-  constructor(props){
+class Greeting extends React.Component {
+  constructor(props) {
     super(props)
 
     this.props.updateType("init")
@@ -15,14 +14,17 @@ import Store from './Client/Store';
 
   }
 
-  render(){
+  render() {
 
     return (
-    <div>
-      <h1 >Darko Store</h1>
-      <Store/>
-    </div>);
+      <div id="home"><br/>
+        <img src={Logo} align='center' alt=''  classes={{
+    justifyContent: 'center',
+    alignItems: 'center',
+  }}/>
+        <Store />
+      </div>);
   }
 }
 
-export default connect (null,{updateType,updateUsername})(Greeting); 
+export default connect(null, { updateType, updateUsername })(Greeting); 
